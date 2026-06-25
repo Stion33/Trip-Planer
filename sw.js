69b7b59be36a310f49fb352f27879aadf1c5d46d
Stion33 <168474266+Stion33@users.noreply.github.com>
2026-06-26 02:53:03 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,34 @@
-Unsupported Media Type
+const CACHE_NAME = 'captain-stion-v20';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './data.json',
+  './icon.svg',
+  './icon-192.png',
+  './icon-512.png',
+  './sea_background.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      if (response.ok && new URL(event.request.url).origin === location.origin) {
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      }
+      return response;
+    }).catch(() => cached))
+  );
+});
