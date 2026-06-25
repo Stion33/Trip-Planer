265745da1c4919a58697abe644642dfa9c8ca9a9
Stion33 <168474266+Stion33@users.noreply.github.com>
2026-06-25 21:43:40 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'seatrip-rental-tabs-v13';
-const APP_ASSETS = ['./', './index.html', './manifest.json', './icon.svg', './icon-192.png', './icon-512.png', './data.json'];
+const CACHE_NAME = 'seatrip-navigation-v19';
+const APP_ASSETS = ['./', './index.html', './manifest.json', './icon.svg', './icon-192.png', './icon-512.png', './data.json', './sea_background.jpg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_ASSETS)).then(() => self.skipWaiting()));
 });
@@ -9,7 +9,7 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
   const url = new URL(event.request.url);
-  if (url.pathname.endsWith('/') || url.pathname.endsWith('/index.html') || url.pathname.endsWith('/manifest.json') || url.pathname.endsWith('/data.json') || url.pathname.endsWith('/icon.svg')) {
+  if (url.pathname.endsWith('/') || url.pathname.endsWith('/index.html') || url.pathname.endsWith('/manifest.json') || url.pathname.endsWith('/data.json') || url.pathname.endsWith('/icon.svg') || url.pathname.endsWith('/sea_background.jpg')) {
     event.respondWith(fetch(event.request, {cache:'no-store'}).then(response => {
       const copy = response.clone();
       caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
